88c104fcbfc99587d635ce9b630c7cdceb7653e6
amir <amirnaderie@gmail.com>
2021-05-31 22:56:41 +0430

    14000310v5

--- public/sw.js
+++ public/sw.js
@@ -4,6 +4,7 @@ const urlsToCache = ["/", "/static/js/bundle.js", "/static/media/2.c1102a94.jpg"
 ,"/favicon.ico"
 ,"/index.html"
 ,"/login"
+,"/rental"
 ];
 
 const self = this;
@@ -23,9 +24,99 @@ self.addEventListener("install", (event) => {
 self.addEventListener("fetch", (event) => {
  // if (!navigator.online) {
     event.respondWith(
-      caches.match(event.request).then(() => {
-        return fetch(event.request).catch(() => caches.match("offline.html"));
+      caches.match(event.request).then((resp) => {
+        if(resp)
+        {
+          return resp;
+        }
+        // return fetch(event.request).catch(() => caches.match("offline.html"));
       })
     );
  // }
 });
+// Activate the SW
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [];
+  cacheWhitelist.push(CACHE_NAME);
+
+  event.waitUntil(
+      caches.keys().then((cacheNames) => Promise.all(
+          cacheNames.map((cacheName) => {
+              if(!cacheWhitelist.includes(cacheName)) {
+                  return caches.delete(cacheName);
+              }
+          })
+      ))
+          
+  )
+});
+
+
+
+// const OFFLINE_VERSION = 1;
+// const CACHE_NAME = 'offline';
+// // Customize this with a different URL if needed.
+// const OFFLINE_URL = ["/", "/static/js/bundle.js", "/static/media/2.c1102a94.jpg"
+// , "/static/media/fontawesome-webfont.af7ae505.woff2"
+// ,"/favicon.ico"
+// ,"/index.html"
+// ,"/login"
+// ,"/rental"
+// ];
+
+// self.addEventListener('install', (event) => {
+//   event.waitUntil((async () => {
+//     const cache = await caches.open(CACHE_NAME);
+//     // Setting {cache: 'reload'} in the new request will ensure that the response
+//     // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
+//     await cache.addAll(OFFLINE_URL);
+//   })());
+// });
+
+// self.addEventListener('activate', (event) => {
+//   event.waitUntil((async () => {
+//     // Enable navigation preload if it's supported.
+//     // See https://developers.google.com/web/updates/2017/02/navigation-preload
+//     if ('navigationPreload' in self.registration) {
+//       await self.registration.navigationPreload.enable();
+//     }
+//   })());
+
+//   // Tell the active service worker to take control of the page immediately.
+//   self.clients.claim();
+// });
+
+// self.addEventListener('fetch', (event) => {
+//   // We only want to call event.respondWith() if this is a navigation request
+//   // for an HTML page.
+//   if (event.request.mode === 'navigate') {
+//     event.respondWith((async () => {
+//       try {
+//         // First, try to use the navigation preload response if it's supported.
+//         const preloadResponse = await event.preloadResponse;
+//         if (preloadResponse) {
+//           return preloadResponse;
+//         }
+
+//         const networkResponse = await fetch(event.request);
+//         return networkResponse;
+//       } catch (error) {
+//         // catch is only triggered if an exception is thrown, which is likely
+//         // due to a network error.
+//         // If fetch() returns a valid HTTP response with a response code in
+//         // the 4xx or 5xx range, the catch() will NOT be called.
+//         console.log('Fetch failed; returning offline page instead.', error);
+
+//         const cache = await caches.open(CACHE_NAME);
+//         const cachedResponse = await cache.matchAll(OFFLINE_URL);
+//         return cachedResponse;
+//       }
+//     })());
+//   }
+
+//   // If our if() condition is false, then this fetch handler won't intercept the
+//   // request. If there are any other fetch handlers registered, they will get a
+//   // chance to call event.respondWith(). If no fetch handlers call
+//   // event.respondWith(), the request will be handled by the browser as if there
+//   // were no service worker involvement.
+// });
